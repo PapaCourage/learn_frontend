2bd5e50a55fdbadb4365380ed613698caf5c5426
Vitalii <marivet25@gmail.com>
2021-03-30 18:07:14 +0300

    first commit

---
+++ 22_js_start/js/script.js
@@ -0,0 +1,35 @@
+" use strict ";
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+	decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+// const isChecked = true,
+//       isClose = true;
+
+// console.log(isChecked && isClose);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked || isClose);
